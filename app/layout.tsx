b764334d2c279e450d96d9e9bea795431bf4b166
Ahmed Hassan <ahmedkiller0234@gmail.com>
2024-10-15 20:17:21 +0500

    EslintErr

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,11 +10,11 @@ const geistSans = localFont({
   weight: "100 900 ",
 });
 
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 
 export const gotham = localFont({
