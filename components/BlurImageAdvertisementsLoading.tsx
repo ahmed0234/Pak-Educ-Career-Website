7d1480aec058611397dc80e4adcc35a8e17c2944
Ahmed Hassan <ahmedkiller0234@gmail.com>
2025-03-19 23:42:49 +0500

    Image Limit Hit

--- components/BlurImageAdvertisementsLoading.tsx
+++ components/BlurImageAdvertisementsLoading.tsx
@@ -1,23 +1,14 @@
 "use client";
-import Image from "next/image";
-import { useState } from "react";
 
 const BlurImageAdvertisementsLoading = ({ university }) => {
-  const [isLoading, setIsLoading] = useState(true);
   return (
-  <div className="relative w-[350px] h-[350px] sm:w-[460px] sm:h-[460px] md:w-[500px] md:h-[500px] lg:w-[550px] lg:h-[550px]">
-      <Image 
-        quality={65} // {number 1-100}
+    <div className="relative w-[350px] h-[350px] sm:w-[460px] sm:h-[460px] md:w-[500px] md:h-[500px] lg:w-[550px] lg:h-[550px]">
+      <img
         alt="Advertisement Pic"
         src={university.hrAdmissionNotice}
-        fill
-        objectFit="contain"
-        className={`transition-all duration-300 ease-in-out  ${
-          isLoading ? "blur-2xl bg-zinc-600" : "blur-0"
-        }`}
-        onLoadingComplete={() => setIsLoading(false)}
+        className="absolute inset-0 w-full h-full object-contain"
       />
-</div>
+    </div>
   );
 };
 
