214e5f303ee17d64e629145fec8778c68d8d839e
Ahmed Hassan <ahmedkiller0234@gmail.com>
2025-03-20 00:07:47 +0500

    images fixing limit hitted

--- components/Admissiontable.tsx
+++ components/Admissiontable.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable */
 
-"use client"
+"use client";
 
 import Link from "next/link";
 import { useState, useEffect } from "react";
@@ -8,7 +8,7 @@ import Sortingtable from "./Sortingtable";
 import { findFilteredUniversities } from "@/actions/filtereduniversities";
 import { DharmaGothicRegular, gotham } from "@/app/layout";
 import Image from "next/image";
-import { Filter } from 'lucide-react';
+import { Filter } from "lucide-react";
 import { SkeletonUI } from "./SkeletonUI";
 
 const AdmissionTableList = ({ university_raw_data, advertisementData }) => {
@@ -18,14 +18,12 @@ const AdmissionTableList = ({ university_raw_data, advertisementData }) => {
   const [selectedProvince, setSelectedProvince] = useState("");
   const [hide, sethide] = useState(true);
   const [allUniversities, setAllUniversities] = useState([]); // Store all universities
-  
+
   // Pagination states
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(50);
   const [totalPages, setTotalPages] = useState(1);
 
-
-  
   function transformUniversityData(data) {
     const today = new Date();
     const transformedData = data.map((university) => {
@@ -34,7 +32,8 @@ const AdmissionTableList = ({ university_raw_data, advertisementData }) => {
       if (university.programs.mphilPrograms.isOpen) programs.push("MPhil");
       if (university.programs.phdPrograms.isOpen) programs.push("PhD");
       if (university.programs.adpPrograms.isOpen) programs.push("ADP");
-      if (university.programs.bs5thPrograms.isOpen) programs.push("BS 5th Semester");
+      if (university.programs.bs5thPrograms.isOpen)
+        programs.push("BS 5th Semester");
       if (university.programs.diplomaPrograms.isOpen) programs.push("Diploma");
 
       return {
@@ -43,7 +42,7 @@ const AdmissionTableList = ({ university_raw_data, advertisementData }) => {
         programs: programs,
         sector: university.sector,
         deadline: new Date(university.admissionDates.deadlineDate),
-        province: university.location.province // Add province for filtering
+        province: university.location.province, // Add province for filtering
       };
     });
 
@@ -92,7 +91,9 @@ const AdmissionTableList = ({ university_raw_data, advertisementData }) => {
   }, [selectedProvince, university_raw_data]);
 
   const updatePagination = (data) => {
-    setTotalPages(itemsPerPage === 'all' ? 1 : Math.ceil(data.length / itemsPerPage));
+    setTotalPages(
+      itemsPerPage === "all" ? 1 : Math.ceil(data.length / itemsPerPage)
+    );
     setCurrentPage(1);
   };
 
@@ -101,11 +102,11 @@ const AdmissionTableList = ({ university_raw_data, advertisementData }) => {
     setCurrentPage(1);
   };
 
-
   async function filteredUniversitiesData(data) {
     setLoading(true);
     const filteredUniversities = await findFilteredUniversities(data);
-    const transformedFilteredUniversities = transformUniversityData(filteredUniversities);
+    const transformedFilteredUniversities =
+      transformUniversityData(filteredUniversities);
     setUniversities(transformedFilteredUniversities);
     updatePagination(transformedFilteredUniversities);
     setLoading(false);
@@ -116,13 +117,14 @@ const AdmissionTableList = ({ university_raw_data, advertisementData }) => {
   };
 
   const handleItemsPerPageChange = (e) => {
-    const newItemsPerPage = e.target.value === 'all' ? 'all' : parseInt(e.target.value);
+    const newItemsPerPage =
+      e.target.value === "all" ? "all" : parseInt(e.target.value);
     setItemsPerPage(newItemsPerPage);
     updatePagination(universities); // Use current filtered universities
   };
 
   const getCurrentUniversities = () => {
-    if (itemsPerPage === 'all') {
+    if (itemsPerPage === "all") {
       return universities;
     }
     const indexOfLastItem = currentPage * itemsPerPage;
@@ -140,11 +142,17 @@ const AdmissionTableList = ({ university_raw_data, advertisementData }) => {
   return (
     <div>
       <div className="HIDE FILTER BOX OR NOT mb-11">
-        <h1 className="border-b border-primary cursor-pointer inline-block select-none max-lg:text-base lg:text-xl xl:text-2xl text-primary font-bold" onClick={() => sethide(!hide)}>
-          Filter University Admissions <span className="inline-block text-center"><Filter size={`20px`}/></span>
+        <h1
+          className="border-b border-primary cursor-pointer inline-block select-none max-lg:text-base lg:text-xl xl:text-2xl text-primary font-bold"
+          onClick={() => sethide(!hide)}
+        >
+          Filter University Admissions{" "}
+          <span className="inline-block text-center">
+            <Filter size={`20px`} />
+          </span>
         </h1>
       </div>
-      
+
       {!hide && (
         <Sortingtable
           filteredUniversitiesData={filteredUniversitiesData}
@@ -153,12 +161,20 @@ const AdmissionTableList = ({ university_raw_data, advertisementData }) => {
         />
       )}
 
-      <div className={loading ? `block text-yellow-400 text-3xl pb-12 italic ${gotham.className}` : "hidden"}>
+      <div
+        className={
+          loading
+            ? `block text-yellow-400 text-3xl pb-12 italic ${gotham.className}`
+            : "hidden"
+        }
+      >
         <SkeletonUI />
       </div>
 
       <div className="flex justify-between items-center mb-4">
-        <h1 className={`glowy dark:glowy_3 max-sm:text-2xl sm:text-4xl border-b inline-block border-rose-600 tracking-wider ${DharmaGothicRegular.className}`}>
+        <h1
+          className={`glowy dark:glowy_3 max-sm:text-2xl sm:text-4xl border-b inline-block border-rose-600 tracking-wider ${DharmaGothicRegular.className}`}
+        >
           List of Universities where Admissions are Open!
         </h1>
       </div>
@@ -166,7 +182,7 @@ const AdmissionTableList = ({ university_raw_data, advertisementData }) => {
       <div className="MainContaineroftable_and_Advertisement flex gap-6 flex-wrap lg:flex-nowrap max-[768px]:flex-col">
         <div className={loading ? "hidden" : `mt-8 overflow-x-auto w-full`}>
           <div className="flex md:justify-between md:items-center mb-4  max-md:flex-col items-start justify-start gap-5">
-            {itemsPerPage !== 'all' && (
+            {itemsPerPage !== "all" && (
               <div className="flex items-center gap-2 max-sm:flex-wrap">
                 <button
                   onClick={() => handlePageChange(currentPage - 1)}
@@ -175,19 +191,21 @@ const AdmissionTableList = ({ university_raw_data, advertisementData }) => {
                 >
                   Previous
                 </button>
-                
+
                 {pageNumbers.map((number) => (
                   <button
                     key={number}
                     onClick={() => handlePageChange(number)}
                     className={`px-3 py-1 border border-primary rounded transition-colors duration-200 ${
-                      currentPage === number ? 'bg-primary text-secondary' : 'text-primary hover:bg-primary hover:text-secondary'
+                      currentPage === number
+                        ? "bg-primary text-secondary"
+                        : "text-primary hover:bg-primary hover:text-secondary"
                     }`}
                   >
                     {number}
                   </button>
                 ))}
-                
+
                 <button
                   onClick={() => handlePageChange(currentPage + 1)}
                   disabled={currentPage === totalPages}
@@ -213,34 +231,60 @@ const AdmissionTableList = ({ university_raw_data, advertisementData }) => {
               </label>
             </div>
           </div>
-       
+
           <table className="min-w-full table-auto rounded-lg border border-primary border-collapse">
             <thead>
               <tr className="bg-primary text-secondary uppercase text-xs md:text-sm leading-normal">
-                <th className="max-[350px]:text-[0.6rem] py-3 px-4 md:px-6 text-left max-md:hidden md:table-cell">Sr.</th>
-                <th className="max-[350px]:text-[0.6rem] py-3 px-4 md:px-6 text-left">University</th>
-                <th className="max-[350px]:text-[0.6rem] py-3 px-4 md:px-6 text-left">Programs</th>
-                <th className="max-[350px]:text-[0.6rem] py-3 px-4 md:px-6 text-left max-md:hidden md:table-cell">Sector</th>
-                <th className="max-[350px]:text-[0.6rem] py-3 px-4 md:px-6 text-left whitespace-nowrap">Deadline</th>
+                <th className="max-[350px]:text-[0.6rem] py-3 px-4 md:px-6 text-left max-md:hidden md:table-cell">
+                  Sr.
+                </th>
+                <th className="max-[350px]:text-[0.6rem] py-3 px-4 md:px-6 text-left">
+                  University
+                </th>
+                <th className="max-[350px]:text-[0.6rem] py-3 px-4 md:px-6 text-left">
+                  Programs
+                </th>
+                <th className="max-[350px]:text-[0.6rem] py-3 px-4 md:px-6 text-left max-md:hidden md:table-cell">
+                  Sector
+                </th>
+                <th className="max-[350px]:text-[0.6rem] py-3 px-4 md:px-6 text-left whitespace-nowrap">
+                  Deadline
+                </th>
               </tr>
             </thead>
             <tbody className="text-xs md:text-sm font-light">
               {currentUniversities.map((university, idx) => (
-                <tr key={university.id} className="border-b border-primary hover:bg-primary hover:text-secondary transition duration-200">
+                <tr
+                  key={university.id}
+                  className="border-b border-primary hover:bg-primary hover:text-secondary transition duration-200"
+                >
                   <td className="py-3 max-md:px-4 md:px-6 relative max-md:hidden md:table-cell">
                     <div className="absolute inset-y-0 left-0 w-px" />
-                    <h1 className="max-md:text-xs md:text-sm">{itemsPerPage === 'all' ? idx + 1 : (currentPage - 1) * itemsPerPage + idx + 1}</h1>
+                    <h1 className="max-md:text-xs md:text-sm">
+                      {itemsPerPage === "all"
+                        ? idx + 1
+                        : (currentPage - 1) * itemsPerPage + idx + 1}
+                    </h1>
                   </td>
                   <td className="py-3 max-md:px-4 md:px-6 relative">
                     <div className="absolute inset-y-0 left-0 w-px border border-primary" />
-                    <Link href={`/university/${university.id}`} className="max-[340px]:text-[0.7rem] sm:text-xs block md:text-sm">{university.name}</Link>
+                    <Link
+                      href={`/university/${university.id}`}
+                      className="max-[340px]:text-[0.7rem] sm:text-xs block md:text-sm"
+                    >
+                      {university.name}
+                    </Link>
                   </td>
                   <td className="py-3 max-md:px-4 md:px-6 relative">
                     <div className="absolute inset-y-0 left-0 w-px border border-primary" />
                     <ul className="flex flex-wrap gap-1 md:gap-1">
                       {university.programs.map((program, idx) => (
-                        <li key={idx} className="inline w-fit text-xs md:text-sm">
-                          {program}{idx < university.programs.length - 1 && ","} 
+                        <li
+                          key={idx}
+                          className="inline w-fit text-xs md:text-sm"
+                        >
+                          {program}
+                          {idx < university.programs.length - 1 && ","}
                         </li>
                       ))}
                     </ul>
@@ -248,14 +292,16 @@ const AdmissionTableList = ({ university_raw_data, advertisementData }) => {
                   <td className="py-3 max-md:px-4 md:px-6 relative max-md:hidden md:table-cell">
                     <div className="absolute inset-y-0 left-0 w-px border border-primary max-md:text-xs md:text-sm" />
                     <h1 className="max-md:text-xs md:text-sm">
-                      {university.sector === 'Government' && 'Govt'}
-                      {university.sector === 'Semi Government' && 'Semi Govt'}
-                      {university.sector === 'Private' && 'Private'}
+                      {university.sector === "Government" && "Govt"}
+                      {university.sector === "Semi Government" && "Semi Govt"}
+                      {university.sector === "Private" && "Private"}
                     </h1>
                   </td>
                   <td className="py-3 max-md:px-4 md:px-6 relative text-red-500 font-semibold sm:whitespace-nowrap">
                     <div className="absolute inset-y-0 left-0 w-px border border-primary" />
-                    <h1 className="max-[340px]:text-[0.65rem] max-md:text-xs md:text-sm">{university.deadline}</h1>
+                    <h1 className="max-[340px]:text-[0.65rem] max-md:text-xs md:text-sm">
+                      {university.deadline}
+                    </h1>
                   </td>
                 </tr>
               ))}
@@ -263,15 +309,20 @@ const AdmissionTableList = ({ university_raw_data, advertisementData }) => {
           </table>
         </div>
 
-        <div className={loading ? "hidden" : "max-sm:mt-6 sm:mt-8 flex flex-wrap max-[1024px]:justify-center lg:justify-start gap-3 lg:flex-col"}>
+        <div
+          className={
+            loading
+              ? "hidden"
+              : "max-sm:mt-6 sm:mt-8 flex flex-wrap max-[1024px]:justify-center lg:justify-start gap-3 lg:flex-col"
+          }
+        >
           {advertisementData.map((ad) => (
             <Link href={`/advertisement/${ad._id}`} key={ad._id}>
               <div className="w-[180px] h-[180px] sm:w-[200px] sm:h-[200px] md:w-[240px] md:h-[240px] xl:w-[280px] xl:h-[280px] 2xl:w-[320px] 2xl:h-[320px] relative top-0 left-0 fancy">
-                <Image
+                <img
                   src={ad.advertisementImg}
                   alt={`Advertisement ${ad.priority}`}
-                  className="object-cover"
-                  fill
+                  className="absolute top-0 left-0 w-full h-full object-cover"
                 />
               </div>
             </Link>
--- components/client/Importantadmission2.jsx
+++ components/client/Importantadmission2.jsx
@@ -1,83 +1,83 @@
-"use client"
+"use client";
 import Link from "next/link";
-import { Navigation, Scrollbar,  Autoplay } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import { Navigation, Scrollbar, Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 import Image from "next/image";
 
-
-
 const Importantadmission2 = ({ data }) => {
-    const handleMouseEnter = (swiper) => {
-      if (swiper && swiper.autoplay) {
-        swiper.autoplay.stop();
-      }
-    };
-  
-    const handleMouseLeave = (swiper) => {
-      if (swiper && swiper.autoplay) {
-        swiper.autoplay.start();
-      }
-    };
-  
-    return (
-      <div className="slider-container mt-8 max-sm:mx-auto">
-        <Swiper
-          spaceBetween={30}
-          slidesPerView={1}
-          navigation={true}
-          pagination={{ clickable: true }}
-          autoplay={{
-            delay: 2000,
-            disableOnInteraction: false,
-          }}
-          loop={true}
-          modules={[Navigation, Autoplay, Scrollbar]}
-          breakpoints={{
-            1280: { slidesPerView: 4 },
-            1024: { slidesPerView: 3 },
-            625: { slidesPerView: 2 },
-          }}
-          onSwiper={(swiper) => {
-            const slides = document.querySelectorAll('.swiper-slide');
-  
-            slides.forEach((slide) => {
-              slide.addEventListener('mouseenter', () => handleMouseEnter(swiper));
-              slide.addEventListener('mouseleave', () => handleMouseLeave(swiper));
-            });
-          }}
-        >
-          {data?.map((university, idx) => (
-            <SwiperSlide key={idx} className="">
-              <Link href={`/university/${university._id}`}>
-                <div className="flex flex-col gap-4 items-center justify-center pb-4 px-2 w-full rounded-xl">
-                  <div className=" h-[400px] w-[300px] relative px-8">
-                    <Image
-                      src={university.hrAdmissionNotice}
-                      alt="Picture"
-                      fill
-                      quality={100}
-                      priority={true}
-                      className="box"
-                    />
-                  </div>
-                  <div className="h-full my-auto flex flex-col gap-6 justify-center">
-                 
-                      <h1 className="ImportantAdmissionUniversityName text-xs">
-                        {university.name}
-                      </h1>
-              
-                  </div>
-                </div>
-              </Link>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-    );
+  const handleMouseEnter = (swiper) => {
+    if (swiper && swiper.autoplay) {
+      swiper.autoplay.stop();
+    }
   };
-  
-  export default Importantadmission2;
+
+  const handleMouseLeave = (swiper) => {
+    if (swiper && swiper.autoplay) {
+      swiper.autoplay.start();
+    }
+  };
+
+  return (
+    <div className="slider-container mt-8 max-sm:mx-auto">
+      <Swiper
+        spaceBetween={30}
+        slidesPerView={1}
+        navigation={true}
+        pagination={{ clickable: true }}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        modules={[Navigation, Autoplay, Scrollbar]}
+        breakpoints={{
+          1280: { slidesPerView: 4 },
+          1024: { slidesPerView: 3 },
+          625: { slidesPerView: 2 },
+        }}
+        onSwiper={(swiper) => {
+          const slides = document.querySelectorAll(".swiper-slide");
+
+          slides.forEach((slide) => {
+            slide.addEventListener("mouseenter", () =>
+              handleMouseEnter(swiper)
+            );
+            slide.addEventListener("mouseleave", () =>
+              handleMouseLeave(swiper)
+            );
+          });
+        }}
+      >
+        {data?.map((university, idx) => (
+          <SwiperSlide key={idx} className="">
+            <Link href={`/university/${university._id}`}>
+              <div className="flex flex-col gap-4 items-center justify-center pb-4 px-2 w-full rounded-xl">
+                <div className="h-[400px] w-[300px] relative">
+                  <img
+                    src={university.hrAdmissionNotice}
+                    alt="Picture"
+                    className="absolute top-0 left-0 w-full h-full object-cover box"
+                    style={{
+                      objectPosition: "center center", // Explicitly set position
+                    }}
+                  />
+                </div>
+                <div className="h-full my-auto flex flex-col gap-6 justify-center">
+                  <h1 className="ImportantAdmissionUniversityName text-xs">
+                    {university.name}
+                  </h1>
+                </div>
+              </div>
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Importantadmission2;
